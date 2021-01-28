60105652743cbf3746598a29e91f1f5a789ecc14
Oleg <espavoru@gmail.com>
2021-01-28 08:59:49 +0200

    Some slider tweaks

--- src/blocks/carousel/carousel.js
+++ src/blocks/carousel/carousel.js
@@ -1,12 +1,29 @@
 const Carousel = (options) => {
   const items = document.querySelectorAll(`${options.cls} .carousel__item`);
   const togglers = document.querySelectorAll(`${options.cls} .carousel__toggler`);
+  let i = 0;
 
   items[0].classList.add('carousel__item--active');
   togglers[0].classList.add('carousel__toggler--active');
 
+  const showNextSlide = () => {
+    i++;
+    if (i >= items.length) i = 0;
+
+    items.forEach(item => {
+      item.classList.remove('carousel__item--active');
+    })
+
+    items[i].classList.add('carousel__item--active');
+
+    togglers.forEach(toggle => {
+      toggle.classList.remove('carousel__toggler--active');
+    })
+    togglers[i].classList.add('carousel__toggler--active');
+  };
+
   const clickHandler = (e) => {
-    if (e.target.closest('.carousel__toggler')) {
+    if (e.target.closest(`${options.cls} .carousel__toggler`)) {
       if (e.target.classList.contains('carousel__toggler--active')) return;
 
       const showSlide = () => {
@@ -29,13 +46,28 @@ const Carousel = (options) => {
 
       showSlide();
     }
+
+    if (e.target.closest(`${options.cls} .carousel__item`)) {
+      showNextSlide();
+    }
+
+
   };
+
+  if (options.autoSlide) {
+    setInterval(() => {
+      showNextSlide();
+    }, 4000);
+  }
 
   document.addEventListener('click', clickHandler);
 };
 
-const reviewSliderOptions = {'cls': '.review'};
+const reviewSliderOptions = {
+  'cls': '.review',
+  'autoSlide': true
+};
 Carousel(reviewSliderOptions);
 
-// const priceSliderOptions = {'cls': '.price'};
-// Carousel(priceSliderOptions);
+const priceSliderOptions = {'cls': '.price'};
+Carousel(priceSliderOptions);
