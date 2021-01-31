3ff8437ef9b41fd40115e5389cd58a58e9071b19
Oleg <espavoru@gmail.com>
2021-01-31 13:01:28 +0200

    Styles for TopNav section (desktop view)

---
+++ src/blocks/top-nav/top-nav.js
@@ -0,0 +1,19 @@
+(() => {
+  const menuLinks = document.querySelectorAll('.top-nav__link');
+
+  const clickHandler = e => {
+    e.preventDefault();
+
+    if (e.target.closest('.top-nav__link')) {
+      if (!menuLinks.length) return;
+      menuLinks.forEach(link => {
+
+        link.classList.remove('top-nav__link--active');
+      })
+
+      e.target.closest('.top-nav__link').classList.add('top-nav__link--active')
+    }
+  };
+
+  document.addEventListener('click', clickHandler);
+})();
